63c2513596d500758f153a1cdec24f56c03d74b9
phneutral <chris@ph-neutral.net>
2019-03-15 17:27:00 +0100

    modulelist

--- deploy/render/__init__.py
+++ deploy/render/__init__.py
@@ -1,5 +1,7 @@
 from server.render import admin, html, json, vi, xml
 
+
+
 @html.utils.jinjaGlobalFilter
 def isList(render, val):
 	return isinstance(val, list)
@@ -7,3 +9,35 @@ def isList(render, val):
 @html.utils.jinjaGlobalFilter
 def isDict(render, val):
 	return isinstance(val, dict)
+
+@html.utils.jinjaGlobalFunction
+def getAdminConf(render):
+	from server import conf
+	import json
+	adminTree = conf["viur.mainApp"]
+
+	adminConfig = {}
+	for key in dir( adminTree ):
+		app = getattr( adminTree, key )
+		if "adminInfo" in dir( app ) and app.adminInfo:
+			if callable( app.adminInfo ):
+				info = app.adminInfo()
+				if info is not None:
+					adminConfig[ key ] = info
+			else:
+				adminConfig[ key ] = app.adminInfo.copy()
+				adminConfig[ key ]["name"] = _(adminConfig[ key ]["name"])
+				adminConfig[ key ]["views"] = []
+				if "views" in app.adminInfo:
+					for v in app.adminInfo["views"]:
+						tmp = v.copy()
+						tmp["name"] = _(tmp["name"])
+						adminConfig[ key ]["views"].append( tmp )
+	res = {	"capabilities": conf["viur.capabilities"],
+		"modules": adminConfig,
+		"configuration": {}
+		}
+	for k, v in conf.items():
+		if k.lower().startswith("admin."):
+			res["configuration"][ k[ 6: ] ] = v
+	return json.dumps( res )
